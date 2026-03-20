e3b5921df0a3120ef78d9b00b24700a2b0793479
Abdelrahman <abdelrahmanfawzyriad@gmail.com>
2026-03-20 19:01:26 +0200

    fix: updated problem 6 solution

--- codeWars_ProblemSolving/problem6.dart
+++ codeWars_ProblemSolving/problem6.dart
@@ -1,3 +1,8 @@
+/*Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8*/
+
 int rowSumOddNumbers(int n) {
   // your code here
   return n * n * n;
